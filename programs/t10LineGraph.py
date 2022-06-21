ddd7ec34ee639712d1669f1009b72449ce918ad8
ck185129 <ck185129@ncr.com>
2022-06-21 10:27:42 -0400

    removed hardcoded length

--- programs/t10LineGraph.py
+++ programs/t10LineGraph.py
@@ -7,14 +7,15 @@ from datetime import datetime as dt
 
 fileName = "../data//pricedata_reshaped.csv"
 data = pd.read_csv(fileName)
+topX = 10
 
 dates = [date for date in data['date']]
 datetimes = []
 for date in dates:
     datetimes.append(dt.strptime(date, '%Y-%m-%d'))
 
-filteredStocks = top_stocks.topStocks("..//data//meta.csv", 10)
-for i in range(10):
+filteredStocks = top_stocks.topStocks("../data/meta.csv", topX)
+for i in range(len(filteredStocks)):
     plt.plot(datetimes, data[filteredStocks['ticker'][filteredStocks.index[i]]], label = filteredStocks['ticker'][filteredStocks.index[i]])
 
 plt.legend(loc="upper left")
