dc0489c7921a7e99b7900d4f0af8d52a69023b5b
ja185332 <ja185332.ncr.com>
2022-07-05 12:57:40 -0400

    repository restructure

--- Cam_Projects/Prime_checker.py
+++
@@ -1,9 +0,0 @@
-def bazooka(num):
-    for i in range(2, num):
-        if num%i == 0:
-            return "Your number is not prime :/ \nIt is divisble by " + str(i)
-
-    return "Your number is prime :)"
-
-user = int(input("Input a number "))
-print(bazooka(user))
