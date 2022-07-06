39e4547c31e064afddcd2b6af19fd0f95c4b956a
ck185129 <ck185129@ncr.com>
2022-07-06 14:17:27 -0400

    added changes

--- apps/binary_predict/README.md
+++ apps/binary_predict/README.md
@@ -8,3 +8,7 @@ Optional:
 * --estimators (int, number of decision trees in random forest)
 * --precision (float, target precision of the model)
 * --predictors (list of strings)
+* --start_date (datetime, mm/dd/yyyy, start of the prediction)
+* --end_date (datetime, mm/dd/yyyy, end of the prediction)
+* --pred_days (int, amount of days predicted using the training)
+* --samples_split (int, higher minimum sample split for faster performance lower precision)
--- apps/binary_predict/binary_predict.py
+++ apps/binary_predict/binary_predict.py
@@ -1,17 +1,20 @@
-from services import predict_buy_sell
+from services import pred_buy_sell
 import argparse
 import datetime as dt
 import matplotlib.pyplot as plt
-import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--ticker', type=str, required=True)
 parser.add_argument('--precision', type=float, required=False)
 parser.add_argument('--estimators', type=int, required=False)
 parser.add_argument('-p', '--predictors', nargs='+', required=False)
+parser.add_argument('--start_date', type=dt.datetime, required=False)
+parser.add_argument('--end_date', type=dt.datetime, required=False)
+parser.add_argument('--pred_days', type=int, required=False)
+parser.add_argument('--samples_split', type=int, required=False)
 args = parser.parse_args()
 
-preds = predict_buy_sell.pred_buy_sell(args.ticker)
+preds = pred_buy_sell(args.ticker)
 
 plt.plot(preds['Predictions'])
 plt.plot(preds['Target'])
